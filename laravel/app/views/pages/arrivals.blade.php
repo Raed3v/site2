d5ffc5697591c5178eadd3762c397bda6e928f8c
Rae <raed3v@gmail.com>
2014-09-09 12:45:35 -0700

    backup 2

---
+++ laravel/app/views/pages/arrivals.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="product-theme">
+      <h2>New Arrivals...</h2>
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg1.jpg">
+        <p>Price: $45.00</p>
+        <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg2.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg3.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg4.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg1.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg2.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg3.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg4.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg1.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg2.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg3.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+      <div class="product-pane">
+        <h3>Item Name</h3>
+        <img class="product-preview" src="http://zamani-flamenco.com/holderimg4.jpg">
+         <p>Price: $45.00</p>
+         <p>Product Details</p>
+      </div>
+
+    </div>
+@stop
---
+++ laravel/app/views/pages/events.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="about-theme">
+      <h2>Events...</h2>
+      <div class="about-pane">
+        <p>Join us in Seattle, WA every spring for our Flamenco Swap Meet. Sign up for our
+           email newsletter for more details:</p>
+        <form class="form-news">
+           <fieldset class="field-news">
+             <input type="email" placeholder="Enter Email Address" required>
+             <button type="submit" class="button-news">Sign Me Up!</button>
+            </fieldset>
+        </form>
+
+
+
+      </div>
+    </div>
+@stop
