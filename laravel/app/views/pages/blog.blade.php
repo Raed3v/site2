7bb1426902861e36a3d348f1796f4ccaa43d51a7
Rae <raed3v@gmail.com>
2014-09-09 14:57:36 -0700

    backup 3

---
+++ laravel/app/views/pages/blog.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="about-theme">
+      <h2>Rastro Flamenco Blog...</h2>
+      <div class="about-pane">
+        <p>Coming soon! Check back for articles with costuming tips and tricks.</p>
+      </div>
+    </div>
+@stop
